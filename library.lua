5d9ede2d9f33265c5aa35c0fce1e17c2ab93f1b9
kumkalah <kumkakumka06@gmail.com>
2026-03-29 23:27:14 +0500

    Update library.lua

--- library.lua
+++ library.lua
@@ -432,7 +432,7 @@ function library:window(properties)
 		size = properties.Size or properties.size or dim2(0, 500, 0, 650),
 	}
 
-	local animated_text = library:animation(cfg.name .. " | fallen survival private        ")
+	local animated_text = library:animation(cfg.name .. " | fallen survival private ")
 
 	-- watermark
 	local __holder = library:create("Frame", {
